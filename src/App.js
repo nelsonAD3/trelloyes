b773bea5b84f7d5323c828e1cd15e30ef6687d3a
Nelson AD <ndemoraes93@gmail.com>
2019-05-29 18:29:47 -0400

    working with callback props

--- src/App.js
+++ src/App.js
@@ -3,28 +3,142 @@ import List from './List'
 import './App.css';
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
+  state = {
+    STORE: {
+      lists: [
+        {
+          id: '1',
+          header: 'First list',
+          cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
+        },
+        {
+          id: '2',
+          header: 'Second list',
+          cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
+        },
+        {
+          id: '3',
+          header: 'Third list',
+          cardIds: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ],
+        },
+        {
+          id: '4',
+          header: 'Fourth list',
+          cardIds: [ 'l', 'm' ],
+        },
+      ],
+      allCards: {
+        'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
+        'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+        'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
+        'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
+        'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
+        'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
+        'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
+        'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
+        'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
+        'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
+        'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
+        'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
+        'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
+      },
     }
   };
 
+  handleDeleteItem(item){
+    console.log('handle delete item called: '+item)
+  }
+  handleAddRandomCard = () => {
+    console.log('handle Add Random Card called: creating random')
+    const newRandomCard = () => {
+      const id = Math.random().toString(36).substring(2, 4)
+        + Math.random().toString(36).substring(2, 4);
+      return {
+        id,
+        title: `Random Card ${id}`,
+        content: 'lorem ipsum',
+      }
+    }
+
+    // handleAddItem = (itemName) => {
+    //   const newItems = [
+    //     ...this.state.shoppingItems,
+    //     { name: itemName, checked: false }
+    //   ]
+    //   this.setState({
+    //     shoppingItems: newItems
+    //   })
+    // }
+    const newStore = {
+      lists: [
+        {
+          id: '1',
+          header: 'First list',
+          cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm', 'z' ],
+        },
+        {
+          id: '2',
+          header: 'Second list',
+          cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
+        },
+        {
+          id: '3',
+          header: 'Third list',
+          cardIds: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ],
+        },
+        {
+          id: '4',
+          header: 'Fourth list',
+          cardIds: [ 'l', 'm' ],
+        },
+      ],
+      allCards: {
+        'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
+        'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+        'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
+        'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
+        'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
+        'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
+        'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
+        'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
+        'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
+        'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
+        'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
+        'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
+        'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
+        'z' : {id: newRandomCard.id, title: newRandomCard.title, content: newRandomCard.content}, 
+      },
+    }
+
+    //newRandomCard won't give me anything.
+
+    
+    this.setState(
+      {STORE: newStore}
+      )
+    
+  }
+
   render() {
-    const { store } = this.props
+    // const { store } = this.props
+    // let storeArray = this.state.STORE.lists;
     return (
       <main className='App'>
         <header className='App-header'>
           <h1>Trelloyes!</h1>
         </header>
         <div className='App-list'>
-          {store.lists.map(list => (
+          {/* {console.log("got here, state is"+this.state)} */}
+          {
+
             <List
-              key={list.id}
-              header={list.header}
-              cards={list.cardIds.map(id => store.allCards[id])}
+              key={this.state.STORE.lists[0].id}
+              header={this.state.STORE.lists[0].header}
+              cards={this.state.STORE.lists[0].cardIds.map(id => this.state.STORE.allCards[id])}
+              onDeleteItem={this.handleDeleteItem}
+              onAddRandomCard={this.handleAddRandomCard}
             />
-          ))}
+          }
         </div>
       </main>
     );
